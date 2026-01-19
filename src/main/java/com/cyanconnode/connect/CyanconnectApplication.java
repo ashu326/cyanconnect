0cc5a12a027caa9fbfbb5a323efd33a60f55f408
shubham-kumar <shubhamkumar10510sk@gmail.com>
2026-01-19 14:13:41 +0530

    Add main Spring Boot application class

---
+++ src/main/java/com/cyanconnode/connect/CyanconnectApplication.java
@@ -0,0 +1,12 @@
+package com.cyanconnode.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyanconnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyanconnectApplication.class, args);
+    }
+}
