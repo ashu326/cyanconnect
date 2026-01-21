5d037561a82dbfaf961a2735d164105a738478ac
shubham-kumar <shubhamkumar10510sk@gmail.com>
2026-01-21 11:55:16 +0530

    handle application startup exception by try-catch block

--- src/main/java/com/cyanconnode/connect/CyanconnectApplication.java
+++ src/main/java/com/cyanconnode/connect/CyanconnectApplication.java
@@ -6,7 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CyanconnectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CyanconnectApplication.class, args);
+    public static void main(String[] args)
+    {
+        try
+        {
+            SpringApplication.run(CyanconnectApplication.class, args);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Application failed to start");
+            e.printStackTrace();
+        }
     }
 }
