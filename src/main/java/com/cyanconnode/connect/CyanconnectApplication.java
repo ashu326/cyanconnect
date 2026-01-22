85431527117fd49230513141933405fb6075f173
shubham710156 <shubhamkumar10510sk@gmail.com>
2026-01-22 12:08:44 +0530

    added initial code setup
    * added gradle
    
    * Add main Spring Boot application class
    
    * check
    
    * handle application startup exception by try-catch block
    
    * Add resources folder with dev profile configuration
    
    ---------
    
    Co-authored-by: rohitkumarcsindia-netizen <rohitkumar.csindia@gmail.com>

---
+++ src/main/java/com/cyanconnode/connect/CyanconnectApplication.java
@@ -0,0 +1,21 @@
+package com.cyanconnode.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyanconnectApplication {
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            SpringApplication.run(CyanconnectApplication.class, args);
+        }
+        catch (Exception e)
+        {
+            System.err.println("Application failed to start");
+            e.printStackTrace();
+        }
+    }
+}
