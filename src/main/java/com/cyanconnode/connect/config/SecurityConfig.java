7caa62c0797a362cfa4426e725dce0203bb26850
shubham710156 <shubhamkumar10510sk@gmail.com>
2026-02-06 11:41:28 +0530

    added get users api
    * create GET method api for users
    
    * all dependencies are grouped in one
    
    * remove Pagable Package and impl offset and limit and impl all possible method in Test package
    
    * remove unneccessary imported packages
    
    * created API for searchByName and impl it's test method also and modify previous test method
    
    * create GET API for Projects and impl it's test method also and add more test method in UserServiceTest class
    
    * Revert "create GET API for Projects and impl it's test method also and add more test method in UserServiceTest class"
    
    This reverts commit 83e2563be0a8c51ef7a74c4b59a8dbb785fbec53.
    
    * add more possible test method  in UserServiceTest
    
    * mapper removed, NameSearch API is modified as suggested and impl other changes as commented
    
    * modify test methods in UserServiceTest and UserControllerTest
    
    * impl SwagegrConfig and SecurityConfig
    
    * modified all changes as commented in PR
    
    * removed userCount in API response
    
    * remove unneccessary and wildcard imports

--- src/main/java/com/cyanconnode/connect/config/SecurityConfig.java
+++ src/main/java/com/cyanconnode/connect/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -27,3 +26,4 @@ public class SecurityConfig
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
     }
 }
+
